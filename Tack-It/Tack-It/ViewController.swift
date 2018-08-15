fca89d588629c2b3e798acd2d9660591244fdaf9
cwille97 <cwille97@gmail.com>
2018-08-15 13:38:59 -0400

    Revisions to the login screen and I set up a TableView for the "cards"

--- Tack-It/Tack-It/ViewController.swift
+++ Tack-It/Tack-It/ViewController.swift
@@ -8,7 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITabBarDelegate, UITableViewDataSource {
+    
+    let list = ["milk", "honey", "apples", "bread"] // a list of items to populate our table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(list.count) // returns the number of items in the list so we know how many rows we need in our table
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell") // gives our cell the default style and assigns it to our prototype cell that we named cell
+        cell.textLabel?.text = list[indexPath.row] // sets the text label as the value in our list at each row
+        return(cell) // return this cell
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,19 +33,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBOutlet weak var userName: UITextField!
     
-    @IBOutlet weak var passWord: UITextField!
+    @IBOutlet weak var userName: UITextField! // variable for the username
+    @IBOutlet weak var passWord: UITextField! // variable for the password
     
     @IBAction func loginSubmit(_ sender: Any) {
-        // put a function call here that will have firebase verify if the user credentials are valid
-        
-        // if user credentials are valid load the main board screen
+        // use Firebase authentication to validate credentials
     }
     
     @IBAction func signUpSubmit(_ sender: Any) {
-        // function call here that will create the user an account with the given credentials and load the main board screen
+        // use Firebase to create the user an account
     }
     
+ 
 }
 
