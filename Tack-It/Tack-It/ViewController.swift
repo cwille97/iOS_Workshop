cd8ecb84e28d1f1e6dbce370b1c8ceecfdbbf1f1
cwille97 <cwille97@gmail.com>
2018-08-15 08:14:38 -0400

    Set up a basic user authentication screen
    Home screen gives the option to sign up or login, firebase functionality coming later.

--- Tack-It/Tack-It/ViewController.swift
+++ Tack-It/Tack-It/ViewController.swift
@@ -19,7 +19,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var passWord: UITextField!
+    
+    @IBAction func loginSubmit(_ sender: Any) {
+        // put a function call here that will have firebase verify if the user credentials are valid
+        
+        // if user credentials are valid load the main board screen
+    }
+    
+    @IBAction func signUpSubmit(_ sender: Any) {
+        // function call here that will create the user an account with the given credentials and load the main board screen
+    }
+    
 }
 
